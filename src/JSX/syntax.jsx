451d80b1f76379a7b6ddfa69a40cd6488bf10651
bharath820 <rambharath820@gmail.com>
2025-07-02 11:08:50 +0530

    jsx

---
+++ src/JSX/syntax.jsx
@@ -0,0 +1,15 @@
+function JsxSyntax(){
+    return (
+<>
+<h1>Jsx Syntax</h1>
+<p>This is a paragraph of JSX</p>
+<img src="image.jpg" alt="A descriptive text" />
+<ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+</>
+    )
+}
+export default JsxSyntax;
