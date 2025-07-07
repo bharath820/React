f5b652af26276a7874b3c519b1ddd2f88bac1897
bharath820 <rambharath820@gmail.com>
2025-07-07 11:15:54 +0530

    msg

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import './App.css'
 import Form from "./Forms/Controlled.jsx"
 import Render from "./Rendering/renderList.jsx"
 import Routes from "./Routes"
+import Hooks from "./Hooks/useState.jsx"
 
 function App() {
 
 
   return (
     <>
-    <Routes />
-      
+    {/* <Routes /> */}
+    <Hooks />
     </>
   )
 }
